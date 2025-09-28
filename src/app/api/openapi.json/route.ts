e01bfb448f6f70a87c2c8e54d63b2539751e1db7
dov-vai <160327869+dov-vai@users.noreply.github.com>
2025-09-28 11:49:38 +0300

    chore: remove redundant openapi route

--- src/app/api/openapi.json/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server";
-import openapi from "../_lib/openapi";
-
-export function GET(_req: NextRequest) {
-  return new Response(JSON.stringify(openapi, null, 2), {
-    headers: { "content-type": "application/json" },
-  });
-}
-
